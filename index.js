1e1396a4c52015a5b03cab4eb1568c0d4e36ddb3
versatile-utility-4919 <jomariepolanco@gmail.com>
2020-09-02 00:17:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toLowerCase());
+  console.log(string.toUpperCase());
 }
 
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!"
